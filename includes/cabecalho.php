61ed60f3f2c4aba51b56316ff4f1f4c0125233b3
Yuri <yurialvesyamada@hotmail.com>
2020-11-04 09:16:35 -0300

    Pagina de Vagas

---
+++ includes/cabecalho.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Estou de Volta</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css">
+    <link rel="stylesheet" href="assets/estilos.css">
+    <!-- Bootstrap CSS -->
+</head>
+
+<body class="bg-primary">
+    <!-- Barra Superior-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">
+            <img src="assets/img/estou-de-volta-2.png" alt="Estou de Volta" width="45px" title="Estou de Volta">
+        </a>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-home"></i> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php"><i class="fas fa-briefcase"></i> Vagas </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-graduation-cap"></i> Capacitação </a>
+                </li>
+            </ul>
+            <!-- Barra de Pesquisa
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            -->
+        </div>
+    </nav>
+    <!-- Fim da Barra Superior-->
+
+    
---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- Conexões com PHP/includes -->
+<?php require("includes/cabecalho.php") ?>
+
+
+<div class="jumbotron jumbotron-fluid text-center">
+        <div class="container">
+        <h1 class="display-3">Estou de Volta </h1>
+        <h2 class="lead h4 my-5">Procure uma oportunidade de emprego</h2>
+        <form class="form mx-auto">
+            <input class="form-control mx-auto" type="search" placeholder="Ex. Segurança, Porteiro." aria-label="Ex. Segurança, Porteiro">
+            <button class="btn btn-primary mx-auto my-3" type="submit">Pesquisar</button>
+        </form>
+        </div>
+</div>
+
+<?php require("includes/rodape.php") ?>
